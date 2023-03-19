ab721fdf733d9e14125785eb2646d5e92e9464f1
Kevin Medri <kevin@netright.co.nz>
2023-03-19 15:34:54 +1300

    Update 2_Cluster_Prediction.py

--- src/tasks/task-5-web-app-deployment/pages/2_Cluster_Prediction.py
+++ src/tasks/task-5-web-app-deployment/pages/2_Cluster_Prediction.py
@@ -253,7 +253,10 @@ def main():
 
         # Display the current cluster group for the selected city.
         cluster = get_cluster(selected_city)
-        st.info(f'Level of Vulnerability: {cluster}', icon='i')
+        result = f'''
+        <p class="res">Level of Vulnerability: {cluster}</p>
+        '''
+        st.markdown(result, unsafe_allow_html=True)
 
         # Display the slider widgets.
         sliders = display_sliders()
